cad7dbf0503c22f70b4fc319a0e3035236be97ae
jason zou <jasonzou@buffalo.edu>
2023-01-19 19:54:58 -0500

    got rid of the scroll bar

--- src/PathFinder.tsx
+++ src/PathFinder.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./PathFinder.css";
 
 function PathFinder() {
   const [count, setCount] = useState(0);
   const [graph, setGraph] = useState([[]]);
 
-  console.log(window.innerHeight);
-  console.log(document.body.clientHeight);
-  return <div className="App">yes</div>;
+  return <div className="App"> yes</div>;
 }
 
 export default PathFinder;
