4d319072fff09166b83ce1128645ca633d396357
jason zou <jasonzou@buffalo.edu>
2023-01-19 20:55:43 -0500

    created an array of objects with properties

--- src/PathFinder.tsx
+++ src/PathFinder.tsx
@@ -4,31 +4,51 @@ import "./PathFinder.css";
 function PathFinder() {
   const ref = useRef<HTMLDivElement>(null);
   const [count, setCount] = useState(0);
-  const [graph, setGraph] = useState([[]]);
+  const [grid, setGrid] = useState<Object[]>([]);
   const [rows, setRows] = useState(0);
   const [columns, setColumns] = useState(0);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
   const square_size = 32;
 
   useEffect(() => {
-    setRows(Math.floor(height / square_size));
-    setColumns(Math.floor(width / square_size));
-  }, [height, width]);
+    setGrid(initGrid(rows, columns));
+  }, [rows, columns]);
 
   useEffect(() => {
     const resizeListener = () => {
       if (ref.current !== null) {
-        console.log(ref);
-        setWidth(ref.current.clientWidth);
-        setHeight(ref.current.clientHeight);
+        setRows(Math.floor(ref.current.clientHeight / square_size));
+        setColumns(Math.floor(ref.current.clientWidth / square_size));
       }
     };
+
+    resizeListener();
     window.addEventListener("resize", resizeListener);
     return () => {
       window.removeEventListener("resize", resizeListener);
     };
   }, []);
+
+  const initGrid = (rows: number, columns: number) => {
+    let newGrid = Array.from({ length: rows }, () => Array(columns).fill(null));
+    for (let row = 0; row < rows; row++) {
+      for (let column = 0; column < columns; column++) {
+        newGrid[row][column] = nodeProps(row, column);
+      }
+    }
+    console.log(newGrid);
+    return newGrid;
+  };
+
+  const nodeProps = (row: number, column: number) => {
+    return {
+      row,
+      column,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+    };
+  };
 
   return (
     <div className="graph-container" ref={ref}>
