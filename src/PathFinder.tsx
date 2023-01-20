1a0b6113187b92f3d7f8f3171e579a208b67df73
jason zou <jasonzou@buffalo.edu>
2023-01-19 20:30:26 -0500

    calculates rows/columns given the size of graph container.

--- src/PathFinder.tsx
+++ src/PathFinder.tsx
@@ -1,11 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./PathFinder.css";
 
 function PathFinder() {
+  const ref = useRef<HTMLDivElement>(null);
   const [count, setCount] = useState(0);
   const [graph, setGraph] = useState([[]]);
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const square_size = 32;
 
-  return <div className="App"> yes</div>;
+  useEffect(() => {
+    setRows(Math.floor(height / square_size));
+    setColumns(Math.floor(width / square_size));
+  }, [height, width]);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      if (ref.current !== null) {
+        console.log(ref);
+        setWidth(ref.current.clientWidth);
+        setHeight(ref.current.clientHeight);
+      }
+    };
+    window.addEventListener("resize", resizeListener);
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+    };
+  }, []);
+
+  return (
+    <div className="graph-container" ref={ref}>
+      {" "}
+      yes
+    </div>
+  );
 }
 
 export default PathFinder;
